1b503354388ef4ef937d4561478191f36e0bcb84
Supradeep13112 <supraroydeep2002@gmail.com>
2025-10-03 02:04:23 +0530

    first sql file

---
+++ sql1new.sql
@@ -0,0 +1,4 @@
+select * from customer where location='New York';
+select first_name,last_name,email from customer where last_name like 'S%';
+select * from customer order by DATE desc;
+select * from customer order by DATE desc limit 5;
